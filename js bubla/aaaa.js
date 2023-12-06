438c76fa44f472b22d39db1cc74ef2b9b2e7c900
Bombonek07 <119570032+Bombonek07@users.noreply.github.com>
2023-12-06 12:46:34 +0100

    aaaaaaaaaa

---
+++ js bubla/aaaa.js
@@ -0,0 +1,12 @@
+let patro = 10
+let out2 = ""
+
+for (let i = 1; i <= patro; i++) {
+    if (i == 1) {
+        out2 += "A"
+    }
+    else {
+        out2 += "AA"
+    } 
+    console.log(`${out2}`)
+}
--- js bubla/nasobilka.js
+++ js bubla/nasobilka.js
@@ -1,4 +1,5 @@
 console.log("hello")
+
 let n = 2
 let out = ""
 
